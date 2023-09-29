3667abd69756fb976749a647a94c79cd89796cb5
Roman Prusakov <prusakovr073@gmail.com>
2023-09-29 18:33:00 +0300

    сделал секцию community

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ $(function() {
     speed: 650
   });
 });
+
+// $(document).on('ready', function() {
+//   $(".warning-sec-slider").slick({
+//     dots: true,
+//     infinite: true,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 6000,
+//     speed: 500,
+//     responsive: [
+//       {
+//         breakpoint: 576, 
+//           settings: {
+//             arrows: false
+//           }
+//       }
+//     ]
+//   });
+// });
